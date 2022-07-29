4084a855a1caafda0b5faf87ef8c98061a20c371
AnthonySaad <Anthonyy.saadd@gmail.com>
2022-07-29 09:59:58 +0300

    Creating New User (Sign-Up) API

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+require("./db");
+const userRouter = require("./routes/user");
+
+const app = express();
+app.use(express.json());
+app.use("/api/user", userRouter);
+
+
+
+
+
+
+app.listen(8000, () => {
+  console.log("the port is listening on port 8000");
+});
